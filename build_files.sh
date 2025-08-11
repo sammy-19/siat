30bca8183eac7fc4cf5d2b99f81ed7e7aefa18e9
Samson Phiri <sammyhills@gmail.com>
2025-08-11 08:25:54 +0300

    reconfigured requirements.txt

--- build_files.sh
+++ build_files.sh
@@ -1,8 +1,4 @@
-# Install dependencies
-echo "Installing dependencies..."
-python3 -m pip install -r requirements.txt
 
+python3.9 -m pip install -r requirements.txt
 
-# Collect static files (to Cloudinary or local)
-echo "Collecting static files..."
 python3.9 manage.py collectstatic
